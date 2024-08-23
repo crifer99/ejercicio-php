cb97057212b25bf9d9b126ec533fd025efa0539f
crifer99 <71573545@idexperujapon.edu.pe>
2024-08-23 18:13:11 -0500

    ejercicio 01

---
+++ operaciones01.php
@@ -0,0 +1,31 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operaciones 01</title>
+</head>
+<body>
+    <h1>La suma es: </h1>
+    <?php
+    $n1 = 50;
+    $n2 = 20;
+    $suma = $n1 + $n2;
+    ?>
+    <p>La suma es: <?php echo $suma; ?> </p>
+    <h1> LA resta es: </h1>
+    <?php
+    $n1 = 70;
+    $n2 = 20;
+    $resta = $n1 - $n2;
+    ?>
+    <p>La resta es: <?php echo $resta; ?> </p>
+    <h1>La multiplicacion es: </h1>
+    <?php
+    $n1 = 20;
+    $n2 = 100;
+    $multiplicacion = $n1 * $n2;
+    ?>
+    <p>La multiplicacion es: <?php echo $multiplicacion; ?> </p>    
+</body>
+</html>
